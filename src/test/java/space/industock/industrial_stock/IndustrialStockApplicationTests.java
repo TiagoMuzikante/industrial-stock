39677d311fb423d70cf6d1b3cdacf649e9e6ea9b
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-09-16 12:36:24 -0300

    Initial commit

---
+++ src/test/java/space/industock/industrial_stock/IndustrialStockApplicationTests.java
@@ -0,0 +1,13 @@
+package space.industock.industrial_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
