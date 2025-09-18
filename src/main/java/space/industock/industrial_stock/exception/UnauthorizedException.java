6c63d962498314c37a86cfbf4707b97409043824
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-09-18 13:26:37 -0300

    create UnauthorizedException for error treatment

---
+++ src/main/java/space/industock/industrial_stock/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package space.industock.industrial_stock.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+  public UnauthorizedException(String message) {
+    super(message);
+  }
+}
