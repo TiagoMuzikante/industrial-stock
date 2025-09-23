eda9418ba37589552613b1b72774178edf4e40ce
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-09-22 23:01:28 -0300

    fix product controller add roles for endpoints and endpoint for historic dashboard

--- src/main/java/space/industock/industrial_stock/controller/ProductController.java
+++ src/main/java/space/industock/industrial_stock/controller/ProductController.java
@@ -4,11 +4,15 @@ import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import space.industock.industrial_stock.domain.ProductHistoric;
 import space.industock.industrial_stock.dto.product.ProductGetDTO;
 import space.industock.industrial_stock.dto.product.ProductPostDTO;
+import space.industock.industrial_stock.dto.productHistory.ProductHistoryDTO;
 import space.industock.industrial_stock.service.ProductManagerService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -20,11 +24,13 @@ public class ProductController {
   private final ProductManagerService service;
 
   @PostMapping
+  @PreAuthorize("hasRole('ADD_STOCK')")
   public ResponseEntity<ProductGetDTO> save(@RequestBody ProductPostDTO productPostDTO){
     return new ResponseEntity<>(service.saveProduct(productPostDTO), HttpStatus.CREATED);
   }
 
   @PatchMapping("/update/{id}")
+  @PreAuthorize("hasRole('ADD_STOCK')")
   public ResponseEntity<ProductGetDTO> replace(@RequestBody ProductPostDTO productPostDTO, @PathVariable Long id){
     return ResponseEntity.ok(service.replaceProduct(productPostDTO, id));
   }
@@ -34,7 +40,9 @@ public class ProductController {
     return ResponseEntity.ok(service.findAllProduct());
   }
 
+
   @PatchMapping("/increment/{id}")
+  @PreAuthorize("hasRole('ADD_STOCK')")
   public ResponseEntity<ProductGetDTO> increment(@PathVariable Long id, @PathParam("amount") Integer amount){
     return ResponseEntity.ok(service.incrementProductAmount(id, amount));
   }
@@ -44,8 +52,11 @@ public class ProductController {
     return ResponseEntity.ok(service.decrementProductAmount(id, amount));
   }
 
-
-
+  @PreAuthorize("hasRole('STOCK_DASHBOARD')")
+  @GetMapping("/resume")
+  public ResponseEntity<List<ProductHistoryDTO>> productHistoricDash(@RequestParam("order_by") String orderBy, @RequestParam("start_date") LocalDate startDate, @RequestParam("end_date") LocalDate endDate){
+    return ResponseEntity.ok(service.findProductHistoric(orderBy, startDate, endDate));
+  }
 
 
 }
