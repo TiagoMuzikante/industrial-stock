90e539e3bbfc25917f8d05e058252911c62b954e
TiagoMuzikante <tiagommuzikante@hotmail.com>
2025-10-14 19:39:45 -0300

    fix auth controller - fix currentUser endPoint

--- src/main/java/space/industock/industrial_stock/controller/AuthController.java
+++ src/main/java/space/industock/industrial_stock/controller/AuthController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +17,7 @@ import space.industock.industrial_stock.dto.auth.LoginRequest;
 import space.industock.industrial_stock.dto.auth.TokenResponse;
 import space.industock.industrial_stock.dto.user.UserGetDTO;
 import space.industock.industrial_stock.mapper.UserMapper;
+import space.industock.industrial_stock.repository.RoleRepository;
 import space.industock.industrial_stock.repository.UserRepository;
 import space.industock.industrial_stock.service.auth.JwtUtil;
 import space.industock.industrial_stock.service.domain.AuthService;
@@ -31,6 +33,7 @@ public class AuthController {
   private final AuthService authService;
   private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
   private final UserMapper userMapper;
   private final JwtUtil jwtUtil;
 
@@ -43,22 +46,37 @@ public class AuthController {
 
   @PostMapping("/new-default-user")
   public ResponseEntity<Void> createUser(){
-    //userRepository.save(new User( null, "minerator", passwordEncoder.encode("!pH6@gTsO9$dsxB"), false, true, true, true, true, "ADD_STOCK, STOCK_DASHBOARD, ADD_GASTO", null));
+   // userRepository.save(new User(
+   //     null,
+   //     "minerator",
+   //     passwordEncoder.encode("!pH6@gTsO9$dsxB"),
+   //     false,
+   //     true,
+   //     true,
+   //     true,
+   //     true,
+   //     null,
+   //     null,
+   //     roleRepository.findByName("ROLE_OWNER").orElseThrow()
+   // ));
     return ResponseEntity.ok().build();
   }
 
   @GetMapping("/current")
   public ResponseEntity<CurrentUser> currentUser(@RequestHeader("Authorization") @Valid String token){
     if(jwtUtil.isTokenExpired(token.substring(7))){
-      return ResponseEntity.ok(new CurrentUser(true, null));
+      return ResponseEntity.ok(new CurrentUser(true, null, null));
     }
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    UserGetDTO user = userMapper.toUserGetDTO(((UserDetailsAdapter) auth.getPrincipal()).getUser());
-    return ResponseEntity.ok(new CurrentUser(jwtUtil.isTokenExpired(token.substring(7)), user));
+
+    UserDetailsAdapter authUser = (UserDetailsAdapter) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    UserGetDTO user = userMapper.toUserGetDTO(authUser.getUser());
+
+    return ResponseEntity.ok(new CurrentUser(jwtUtil.isTokenExpired(token.substring(7)), authUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList(), user));
   }
 
+  // debug
   @GetMapping("/current-user")
-  public ResponseEntity<User> currentUserFull(@RequestHeader("Authorization") @Valid String token){
+  public ResponseEntity<User> currentUserFull(){
     Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     return ResponseEntity.ok(((UserDetailsAdapter) auth.getPrincipal()).getUser());
   }
